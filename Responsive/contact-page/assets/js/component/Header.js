e5a58938101fb6584ef7bda854084c329187d6d3
Wanderlee Max <wanderleedev@gmail.com>
2023-10-01 19:50:05 -0500

    challenge 4 terminado

--- Responsive/contact-page/assets/js/component/Header.js
+++ Responsive/contact-page/assets/js/component/Header.js
@@ -2,11 +2,11 @@ const Header = () => {
   return (`
     <header class="grid gap-16 w-full">
       <img class="justify-self-start" src="/assets/img/contact-form-icon.svg" alt="icon-form">
-      <div class="w-header text-center justify-self-center grid gap-4 lg:w-[30rem]">
-        <h1 class="text-4xl font-black leading-snug lg:text-5xl lg:leading-normal">
+      <div class="justify-self-center text-center grid gap-4">
+        <h1 class="w-header text-4xl font-black w-header leading-snug md:text-[4rem] justify-self-center">
           Interested in our business pricing?
         </h1>
-        <p class="font-medium lg:text-xl">
+        <p class="font-medium leading-snug lg:text-3xl md:leading-relaxed md:w-[50rem] justify-self-center">
           Fill out the form to view details and we’ll contact you as soon as possible.
         </p>
       </div>
