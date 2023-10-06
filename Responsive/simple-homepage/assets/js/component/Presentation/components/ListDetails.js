55483bffa41997104aefcea2b4f0c2a0bfa1c207
Wanderlee Max <wanderleedev@gmail.com>
2023-10-06 11:40:51 -0500

    Challenge 5 terminado

--- Responsive/simple-homepage/assets/js/component/Presentation/components/ListDetails.js
+++ Responsive/simple-homepage/assets/js/component/Presentation/components/ListDetails.js
@@ -5,7 +5,7 @@ const ListDetails = () => {
   ]
 
   return (`
-    <ul class="flex gap-8 text-[var(--a)] text-sm">
+    <ul class="flex gap-8 text-[var(--text-clr)] text-sm font-semibold">
       ${details.map( detail => (`
         <li class="grid grid-cols-[auto_1fr] items-center gap-2">
           <span class="bg-imageBasic w-8 h-8 block"></span>
